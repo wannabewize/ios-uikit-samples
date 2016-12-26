6eb848f29d3bf9e3db224da87c1bce4d75ceab18
wannabewize <wannabewize@gmail.com>
2016-12-27 02:03:53 +0900

    이미지 스트레칭 예제

---
+++ 03_SceneAndContents/ViewStretch/ViewStretch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewStretch
+//
+//  Created by wannabewize on 2016. 12. 27..
+//  Copyright © 2016년 VanillaStep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
