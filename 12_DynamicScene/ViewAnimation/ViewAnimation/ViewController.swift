b992705afe22818f81ffa37335b079022897e62b
wannabewize <wannabewize+macbookpro@gmail.com>
2016-12-30 22:47:24 +0900

    Sprint Animation 추가

--- 12_DynamicScene/ViewAnimation/ViewAnimation/ViewController.swift
+++ 12_DynamicScene/ViewAnimation/ViewAnimation/ViewController.swift
@@ -29,6 +29,20 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func springBlock(_ sender : Any) {
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.3, options: .curveEaseIn, animations: { 
+            let width = self.view.frame.size.width - self.block.frame.size.width
+            let height = self.view.frame.height - self.block.frame.height - 44
+            
+            let x = arc4random_uniform(UInt32(width))
+            let y = arc4random_uniform(UInt32(height))
+            
+            self.block.center = CGPoint(x : CGFloat(x), y: CGFloat(y))
+        }) { (finished : Bool) in
+            print("Finished")
+        }
+    }
+    
     @IBAction func animateMulti(_ sender: Any) {
         // do it!
         UIView.animate(withDuration: 1.0, animations: { 
