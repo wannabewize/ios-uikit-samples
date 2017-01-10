faaf1a7f4bbb4027386d39eed01349ccb337c3bb
wannabewize <wannabewize+macbookpro@gmail.com>
2017-01-11 01:10:27 +0900

    네트워크 라이브러리

--- 17_Network/NetworkLibraries/NetworkLibraries/ViewController.swift
+++ 17_Network/NetworkLibraries/NetworkLibraries/ViewController.swift
@@ -4,21 +4,70 @@ import AlamofireImage
 
 class ViewController: UIViewController {
     
-    let urlStr = "http://lorempixel.com/800/600/cats/"
+    let imageUrl = "http://lorempixel.com/800/600/cats/"
+    // cat 이미지가 expectedLength가 없어서 프로그레스 뷰 적용이 안됨
+    let screamUrl = "http://www.ibiblio.org/wm/paint/auth/munch/munch.scream.jpg"
+    let jsonUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=25/json"
     
-    func dataTask() {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    func loadImage1() {
+        if let url = URL(string: imageUrl) {
+            imageView.af_setImage(withURL: url)
+        }
+    }
+    
+    // Load image with Placeholder
+    func loadImage2() {
+        if let url = URL(string: imageUrl) {
+            let placeHolderImage = UIImage(named: "logo")
+            imageView.af_setImage(withURL: url, placeholderImage: placeHolderImage)
+        }
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func loadImage(_ sender: Any) {
+//        loadImage1()
+        loadImage2()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func requestJSON(_ sender: Any) {
+        textView.text = ""
+        
+        Alamofire.request(jsonUrl).responseJSON { (response : DataResponse<Any>) in
+            
+            if response.result.isSuccess,
+                let root = response.result.value as? [String:Any],
+                let feed = root["feed"] as? [String:Any],
+                let entries = feed["entry"] as? [ [String:Any] ] {
+                for entry in entries {
+                    let title = entry["title"] as! [String:String]
+                    self.textView.text.append(title["label"]! + "\n")
+                }
+            }
+        }
     }
-
-
+    
+    @IBAction func requestImage(_ sender: Any) {
+        progressView.progress = 0
+        
+        Alamofire.request(screamUrl)
+            .downloadProgress(closure: { (progress : Progress) in
+                print(progress.fractionCompleted)
+                self.progressView.progress = Float(progress.fractionCompleted)
+            })
+            .responseImage { (response : DataResponse<Image>) in
+            if response.result.isSuccess,
+                let image = response.result.value {
+                self.imageView.image = image
+            }
+        }
+    }
+    
+    @IBAction func clearCache(_ sender: Any) {
+        URLCache.shared.removeAllCachedResponses()
+    }
 }
 
