028ca093bf5bd2be476d87f1e30d386b086a3b1c
wannabewize <wannabewize@gmail.com>
2017-01-31 02:05:37 +0900

    아답티브 UI 예제.
    이미지 셋을 이용한 아답티브 UI

--- 04_Layout/AdaptiveUI/AdaptiveUI/ViewController.swift
+++ 04_Layout/AdaptiveUI/AdaptiveUI/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.traitCollection
         // Do any additional setup after loading the view, typically from a nib.
     }
 
