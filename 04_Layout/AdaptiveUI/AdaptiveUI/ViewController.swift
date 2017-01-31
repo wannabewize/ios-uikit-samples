8bf8c51cde1ca09ce2537ef452f6043d1dd6a0fe
wannabewize <wannabewize@gmail.com>
2017-01-31 23:51:53 +0900

    적응형 UI 예제

--- 04_Layout/AdaptiveUI/AdaptiveUI/ViewController.swift
+++ 04_Layout/AdaptiveUI/AdaptiveUI/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.traitCollection
         // Do any additional setup after loading the view, typically from a nib.
     }
 
