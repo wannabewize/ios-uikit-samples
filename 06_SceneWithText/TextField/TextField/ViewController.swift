8a4d93478ef61065ed39b4d1040a478568b8978c
wannabewize <wannabewize@gmail.com>
2017-02-02 02:14:32 +0900

    텍스트 필드 예제

--- 06_SceneWithText/TextField/TextField/ViewController.swift
+++ 06_SceneWithText/TextField/TextField/ViewController.swift
@@ -9,19 +9,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let textField = UITextField(frame: CGRect(x: 20, y: 50, width: 300, height: 50))
-        // 안내 문구
-        textField.placeholder = "여기에 메일 주소를 입력하세요"
+        let textField = UITextField(frame: CGRect(x:0, y:50, width:self.view.frame.size.width, height:50))
+        self.view.addSubview(textField)
+        
         // 폰트
         textField.font = UIFont.systemFont(ofSize: 20)
         // 둥근 사각형 테두리
         textField.borderStyle = UITextBorderStyle.roundedRect
-        // 편집 상태일 때 내용 삭제 버튼
+        // 편집 상태가 되면 내용 삭제 버튼 활성화
         textField.clearsOnBeginEditing = true
-        // URL 입력용 키보드
-        textField.keyboardType = UIKeyboardType.URL
-        // 부 구조에 추가
-        self.view.addSubview(textField)
+        
+        // 안내 문구
+        textField.placeholder = "여기에 메일 주소를 입력하세요"
+        // 키보드는 email 입력용
+        textField.keyboardType = UIKeyboardType.emailAddress
+        
+        // 스펠링 체크 OFF
+        textField.spellCheckingType = .yes
+        // 자동 고치기 OFF
+        textField.autocorrectionType = .no
+        // 자동 대문자 변경 OFF
+        textField.autocapitalizationType = .none
+        
+        // 엔터로 편집 상태 종료
+        textField.addTarget(self, action: #selector(handleDidEnd), for: .editingDidEndOnExit)
+    }
+    
+    func handleDidEnd() {
+        print("did end")
     }
 }
 
