583c6f4c65a4abca4cb0d9a027c759f6716ebecf
wannabewize <wannabewize@gmail.com>
2017-02-08 02:16:11 +0900

    구조체로 데이터 다루기

--- 11_GridScene/CollectionView/CollectionView/ViewController.swift
+++ 11_GridScene/CollectionView/CollectionView/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
+struct Nation {
+    var name : String
+    var flag : UIImage?
+}
+
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    var nations : [[String:String]]!
+    var nations : [Nation]!
     
     // Selection : label.highlightedTextColor
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("Select Item : " + (nations[indexPath.row]["NAME"])!);
+        print("Select Item : \(nations[indexPath.row].name)");
+    }
+    
+    // cell lifecycle
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        print("willDisplay \(indexPath)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        print("didEndDisplaying \(indexPath)")
     }
     
     
@@ -23,17 +37,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        print("cellForItemAt : \(indexPath)")
         // 셀 객체 찾기
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FLAG_CELL", for: indexPath) as! FlagCell
         
         // 셀에 해당하는 객체
-        let flag = nations[indexPath.item]
-        let fileName = flag["IMAGE"]!
-        let name = flag["NAME"]
+        let nation = nations[indexPath.item]
         
         // 내용 반영
-        cell.imageView.image = UIImage(named: fileName)
-        cell.nameLabel.text = name
+        cell.nameLabel.text = nation.name
+        cell.imageView.image = nation.flag
         
         return cell
     }
@@ -42,19 +55,19 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         super.viewDidLoad()
             
         nations = [
-            [ "NAME":"Korea", "IMAGE":"Korea.png" ],
-            [ "NAME":"Japan", "IMAGE":"Japan.png" ],
-            [ "NAME":"Philippines", "IMAGE":"Philippines.png" ],
-            [ "NAME":"Thailand", "IMAGE":"Thailand.png" ],
-            [ "NAME":"Vietnam", "IMAGE":"Vietnam.png" ],
-            [ "NAME":"Denmark", "IMAGE":"Denmark.png" ],
-            [ "NAME":"Germany", "IMAGE":"Germany.png" ],
-            [ "NAME":"Greece", "IMAGE":"Greece.png" ],
-            [ "NAME":"Italy", "IMAGE":"Italy.png" ],
-            [ "NAME":"Norway", "IMAGE":"Norway.png" ],
-            [ "NAME":"Spain", "IMAGE":"Spain.png" ],
-            [ "NAME":"Turkey", "IMAGE":"Turkey.png" ],
-            [ "NAME":"United Kingdom", "IMAGE":"United_Kingdom.png" ]
+            Nation(name: "Korea", flag: #imageLiteral(resourceName: "Korea.png")),
+            Nation(name: "Japan", flag: #imageLiteral(resourceName: "Japan.png")),
+            Nation(name: "Philippines", flag: #imageLiteral(resourceName: "Philippines.png")),
+            Nation(name: "Thailand", flag: #imageLiteral(resourceName: "Thailand.png")),
+            Nation(name: "Vietnam", flag: #imageLiteral(resourceName: "Vietnam.png")),
+            Nation(name: "Denmark", flag: #imageLiteral(resourceName: "Denmark.png")),
+            Nation(name: "Germany", flag: #imageLiteral(resourceName: "Germany.png")),
+            Nation(name: "Greece", flag: #imageLiteral(resourceName: "Greece.png")),
+            Nation(name: "Italy", flag: #imageLiteral(resourceName: "Italy.png")),
+            Nation(name: "Norway", flag: #imageLiteral(resourceName: "Norway.png")),
+            Nation(name: "Spain", flag: #imageLiteral(resourceName: "Spain.png")),
+            Nation(name: "Turkey", flag: #imageLiteral(resourceName: "Turkey.png")),
+            Nation(name: "United Kingdom", flag: #imageLiteral(resourceName: "United_Kingdom.png"))
         ]
     }
 
