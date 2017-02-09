b85bf417ca9356111faaf98c6f7c7389fb55365a
wannabewize <wannabewize@gmail.com>
2017-02-10 01:37:45 +0900

    이미지 뷰의 애니메이션 예제

--- 12_DynamicScene/ImageViewAnimation/ImageViewAnimation/ViewController.swift
+++ 12_DynamicScene/ImageViewAnimation/ImageViewAnimation/ViewController.swift
@@ -12,11 +12,33 @@ class ViewController: UIViewController {
     
     @IBOutlet var imageView : UIImageView!
 
+    @IBAction func startAnimation(_ sender: Any) {
+        imageView.startAnimating()
+    }
+    
+    @IBAction func stopAnimation(_ sender: Any) {
+        if imageView.isAnimating {
+            imageView.stopAnimating()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let images = [
+            UIImage(named: "0.png")!,
+            UIImage(named: "1.png")!,
+            UIImage(named: "2.png")!,
+            UIImage(named: "3.png")!,
+            UIImage(named: "4.png")!,
+            UIImage(named: "5.png")!,
+            UIImage(named: "6.png")!,
+            UIImage(named: "7.png")!,
+            UIImage(named: "8.png")!,
+            UIImage(named: "9.png")!
+        ]
         
-        
-        imageView.animationImages
+        imageView.animationImages = images
+        imageView.animationDuration = 3
     }
 
     override func didReceiveMemoryWarning() {
