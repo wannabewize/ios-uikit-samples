20621dd029f80739df2c92020802137e43007162
wannabewize <wannabewize@gmail.com>
2017-02-10 03:05:26 +0900

    스프링 애니메이션 분리

--- 12_DynamicScene/ViewAnimation/ViewAnimation/ViewController.swift
+++ 12_DynamicScene/ViewAnimation/ViewAnimation/ViewController.swift
@@ -9,6 +9,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var block: UIView!
     
+    @IBAction func changeColor(_ sender: Any) {
+        let r = CGFloat(arc4random_uniform(255)) / 255
+        let g = CGFloat(arc4random_uniform(255)) / 255
+        let b = CGFloat(arc4random_uniform(255)) / 255
+        
+//        UIView.animate(withDuration: 1.0) {
+//            self.block.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+//        }
+        UIView.animate(withDuration: 1.0, delay: 0, options: [.curveEaseIn, .repeat, .autoreverse], animations: {
+            self.block.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        }) { (finished : Bool) in
+            print("completed")
+        }
+        
+    }
+    
     @IBAction func moveBlock(_ sender: Any) {
         UIView.animate(withDuration: 1.0) { 
             let width = self.view.frame.size.width - self.block.frame.size.width
@@ -29,23 +45,10 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func springBlock(_ sender : Any) {
-        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.3, options: .curveEaseIn, animations: { 
-            let width = self.view.frame.size.width - self.block.frame.size.width
-            let height = self.view.frame.height - self.block.frame.height - 44
-            
-            let x = arc4random_uniform(UInt32(width))
-            let y = arc4random_uniform(UInt32(height))
-            
-            self.block.center = CGPoint(x : CGFloat(x), y: CGFloat(y))
-        }) { (finished : Bool) in
-            print("Finished")
-        }
-    }
     
     @IBAction func animateMulti(_ sender: Any) {
         // do it!
-        UIView.animate(withDuration: 1.0, animations: { 
+        UIView.animate(withDuration: 1.0, animations: {
             let width = self.view.frame.size.width - self.block.frame.size.width
             let height = self.view.frame.height - self.block.frame.height - 44
             
